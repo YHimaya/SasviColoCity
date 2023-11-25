36a7c098bf27577933d40d28eda7e20127925fd8
Yasasvi Himaya <139344990+YHimaya@users.noreply.github.com>
2023-11-25 18:34:45 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
         
         function scrollFunction() {
-          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
             document.getElementById("navbar").style.top = "0";
           } else {
             document.getElementById("navbar").style.top = "-100px";
